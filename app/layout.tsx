3445fa39b3707810db07419b206a2d5d84841950
Ayush <ayushedith@gmail.com>
2025-02-21 11:59:57 +0530

    update layout component; change Open Graph and Twitter meta tags to reflect new URL and image

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,18 +23,18 @@ export default function RootLayout({
                 <link href="/favicon/apexia-light.svg" rel="icon" type="image/svg" media="(prefers-color-scheme: light)" />
                 <link href="/favicon/apexia-dark.svg" rel="icon" type="image/svg" media="(prefers-color-scheme: dark)" />
                 <meta name="description" content="Apexia is hosting a mini-version of figbruary. It will be the same concept as figbruary but, will go on for 7 Days." />
-                <meta property="og:url" content="https://figbruary.apexia.club/" />
+                <meta property="og:url" content="https://ideathon-web.vercel.app/" />
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Apexia Figbruary" />
                 <meta property="og:description" content="Apexia is hosting a mini-version of figbruary. It will be the same concept as figbruary but, will go on for 7 Days." />
-                <meta property="og:image" content="https://figbruary.apexia.club/OG_Image.png" />
+                <meta property="og:image" content="https://ideathon-web.vercel.app/ideathonimg1.png" />
 
                 <meta name="twitter:card" content="summary_large_image" />
-                <meta property="twitter:domain" content="figbruary.apexia.club" />
-                <meta property="twitter:url" content="https://figbruary.apexia.club/" />
+                <meta property="twitter:domain" content="ideathon-web.vercel.app" />
+                <meta property="twitter:url" content="https://ideathon-web.vercel.app/" />
                 <meta name="twitter:title" content="Apexia" />
                 <meta name="twitter:description" content="Bits to brilliance, together!" />
-                <meta name="twitter:image" content="https://figbruary.apexia.club/OG_Image.png" />
+                <meta name="twitter:image" content="https://ideathon-web.vercel.app/ideathonimg1.png" />
 
                 {/* google Analytics */}
                 <Script async src="https://www.googletagmanager.com/gtag/js?id=<YOUR-G-TAG>"></Script>
